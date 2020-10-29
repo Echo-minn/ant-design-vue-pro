10a61e57ca6ff5c2e4a89a8eea9fa0738b983315
Echo-minn <minn798088@gmail.com>
2020-10-29 13:59:52 +0800

    跨域本地代理设置

--- vue.config.js
+++ vue.config.js
@@ -105,15 +105,13 @@ const vueConfig = {
 
   devServer: {
     // development server port 8000
-    port: 8000
+    port: 8099,
     // If you want to turn on the proxy, please remove the mockjs /src/main.jsL11
-    // proxy: {
-    //   '/api': {
-    //     target: 'https://mock.ihx.me/mock/5baf3052f7da7e07e04a5116/antd-pro',
-    //     ws: false,
-    //     changeOrigin: true
-    //   }
-    // }
+    proxy: {
+      '/api': {
+        target: 'http://emergingniche.com:8099',
+      }
+    }
   },
 
   // disable source map in production
